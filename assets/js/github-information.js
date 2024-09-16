b7f43724108060e7bd6270c4c6d59397773e450d
Andrea Nilsson <andreaycoral@icloud.com>
2024-09-16 12:51:16 +0000

    Fix bugs regarding empty divs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -35,7 +35,9 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
-
+    //to empty divs use empty string
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -68,3 +70,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document.readyState(fetchGitHubInformation);
